e3ca909b7ca3108c88f9ff948a63f9e8bd1b2643
LogicHou <logichou@qq.com>
2022-03-24 13:40:27 +0800

    修个启动页面一些选项在windows下的路径

--- lua/plugin-config/dashboard.lua
+++ lua/plugin-config/dashboard.lua
@@ -4,8 +4,10 @@ vim.g.dashboard_custom_footer = { "https://github.com/logichou/nvim" }
 vim.g.dashboard_custom_section = {
   a = { description = { "  Projects              " }, command = "Telescope projects" },
   b = { description = { "  Recently files        " }, command = "Telescope oldfiles" },
-  c = { description = { "  Edit keybindings      " }, command = "edit ~/.config/nvim/lua/keybindings.lua" },
-  d = { description = { "  Edit Projects         " }, command = "edit ~/.local/share/nvim/project_nvim/project_history", },
+  c = { description = { "  Edit keybindings      " }, command = "edit C:/Users/hou/AppData/Local/nvim/lua/keybindings.lua" },
+  d = { description = { "  Edit Projects         " }, command = "edit C:/Users/hou/AppData/Local/nvim-data/project_nvim/project_history", },
+  -- c = { description = { "  Edit keybindings      " }, command = "edit ~/.config/nvim/lua/keybindings.lua" },
+  -- d = { description = { "  Edit Projects         " }, command = "edit ~/.local/share/nvim/project_nvim/project_history", },
   -- e = { description = { "  Edit .bashrc          " }, command = "edit ~/.bashrc" },
   -- f = { description = { "  Edit init.lua         " }, command = "edit ~/.config/nvim/init.lua" },
   -- g = { description = {'  Find file          '}, command = 'Telescope find_files'},
