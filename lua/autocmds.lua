dc1ab7c9cfa77df887c94a9f87752fef0eb0d265
LogicHou <logichou@qq.com>
2022-04-16 19:06:08 +0800

    自动格式化适配js ts css文件

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -38,7 +38,7 @@ autocmd('TermOpen', {
 -- 保存时自动格式化
 autocmd('BufWritePre', {
     group = myAutoGroup,
-    pattern = { '*.lua', '*.py', '*.go' },
+    pattern = { '*.lua', '*.py', '*.go', '*.js', '*.ts', '*.css' },
     callback = vim.lsp.buf.formatting_sync,
 })
 
